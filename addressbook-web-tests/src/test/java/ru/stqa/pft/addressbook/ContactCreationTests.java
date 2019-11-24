1a9dc22699b301b4205d99cbdf053227c3a9299e
iakovenm <iakovenko.maria@gmail.com>
2019-11-24 19:44:27 +0100

    Task4: added before, after Method correction

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -10,7 +10,7 @@ public class ContactCreationTests {
     private WebDriver wd;
 
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -63,7 +63,7 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
 
     public void tearDown() throws Exception {
         wd.quit();
